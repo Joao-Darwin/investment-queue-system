e7ee0a9907e9015b306c7fcdb79f9afb2c4c5db3
João Darwin <joaodarwin.ip22@gmail.com>
2025-02-26 01:00:46 -0300

    feat: Criar serviço de RabbitMQ com método para salvar payload na fila

---
+++ producer/src/main/java/com/producer/services/RabbitMQService.java
@@ -0,0 +1,27 @@
+package com.producer.services;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQService {
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    private final RabbitTemplate rabbitTemplate;
+
+    @Autowired
+    public RabbitMQService(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(Object object) {
+        rabbitTemplate.convertAndSend(exchange, routingKey, object);
+    }
+}
+
